5bb4a6ef5fbabaf70ca7e1f2c5e330c3acd4e681
Frederico Tavares <fredericotp@hotmail.com>
2023-06-13 13:51:22 -0300

    Feat: Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if ".txt" not in path_file:
+        print("Formato inválido", file=sys.stderr)
+        return
+
+    try:
+        with open(path_file) as file:
+            lines = []
+            for line in file:
+                new_line = line.replace("\n", "")
+                lines.append(new_line)
+
+        return lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return

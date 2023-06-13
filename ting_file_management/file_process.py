0e233ddd8e32a212325ab4975d633fae6422ecd5
Frederico Tavares <fredericotp@hotmail.com>
2023-06-13 14:09:33 -0300

    Feat: Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+
+    for index in range(len(instance)):
+        element = instance.search(index)
+        if element == new_dict:
+            return
+
+    instance.enqueue(new_dict)
+    print(new_dict, file=sys.stdout)
 
 
 def remove(instance):

af318cd362e6dbaa9609f64c2fef16cf52877870
Frederico Tavares <fredericotp@hotmail.com>
2023-06-13 14:44:03 -0300

    Feat: Requisito 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,35 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
+
+
+is_priority = {
+    "nome_do_arquivo": "arquivo_teste.txt",
+    "qtd_linhas": 3,
+    "linhas_do_arquivo": [...],
+}
+
+not_priority = {
+    "nome_do_arquivo": "arquivo_teste.txt",
+    "qtd_linhas": 7,
+    "linhas_do_arquivo": [...],
+}
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+    queue.enqueue(not_priority)
+    queue.enqueue(not_priority)
+    queue.enqueue(is_priority)
+    queue.enqueue(not_priority)
+    queue.enqueue(is_priority)
+
+    assert len(queue) == 5
+
+    returned_value = queue.dequeue()
+
+    assert returned_value == is_priority
+    assert queue.search(0) == is_priority
+    assert queue.search(3) == not_priority
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(10)
